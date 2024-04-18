643a5937b632afe2512568a4c8efc5206e07d7a5
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-04-18 13:11:29 +0530

    created login api.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user.model');
 const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const SignUp = async (req, res, next) => {
     try {
@@ -23,3 +24,27 @@ const SignUp = async (req, res, next) => {
 }
 
 module.exports = SignUp;
+
+const Login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (user) {
+            const isMatch = await bcryptjs.compare(password, user.password);
+            if (isMatch) {
+                const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+                const { password, ...others } = user._doc;
+                const expiryDate = new Date(Date.now() + 3600000);
+                return res.cookie('access_token', token, { httpOnly: true, expires: expiryDate }).status(200).json(others);
+            } else {
+                return res.status(400).json({ message: "Invalid credentials" });
+            }
+        } else {
+            return res.status(400).json({ message: "User not found" });
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = Login;
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const SignUp = require('../controllers/auth.controller');
+const Login = require('../controllers/auth.controller');
 const router = express.Router();
 
 router.post('/Signup', SignUp);
+router.post('/Login', Login);
 
 module.exports = router;
