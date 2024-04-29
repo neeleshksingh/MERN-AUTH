cee22e97dabd8e667b9e3838bbe3011ed62f05ed
Neelesh Kumar Singh <neelesh1517@gmail.com>
2024-04-29 21:50:52 +0530

    Created Google Authentication api.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -48,3 +48,33 @@ const Login = async (req, res, next) => {
 }
 
 module.exports = Login;
+
+const Google = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        if (user) {
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+            const { password, ...others } = user._doc;
+            const expiryDate = new Date(Date.now() + 3600000);
+            return res.cookie('access_token', token, { httpOnly: true, expires: expiryDate }).status(200).json(others);
+        } else {
+            const generatePassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+            const hashedPassword = await bcryptjs.hash(generatePassword, 10);
+            const newUser = new User({
+                username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-8),
+                email: req.body.email,
+                password: hashedPassword,
+                profilePicture: req.body.photo
+            });
+            await newUser.save();
+            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+            const { password, ...others } = newUser._doc;
+            const expiryDate = new Date(Date.now() + 3600000);
+            return res.cookie('access_token', token, { httpOnly: true, expires: expiryDate }).status(200).json(others);
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = Google;
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const SignUp = require('../controllers/auth.controller');
 const Login = require('../controllers/auth.controller');
+const Google = require('../controllers/auth.controller');
 const router = express.Router();
 
 router.post('/Signup', SignUp);
 router.post('/Login', Login);
+router.post('/auth-google', Google);
 
 module.exports = router;
